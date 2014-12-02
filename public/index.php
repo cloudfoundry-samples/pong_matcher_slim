c1cc234c8bd1a1024ecac5c80c776d8bfb7644ce
Andrew Bruce <andrew@camelpunch.com>
2014-12-02 14:33:05 +0000

    Passing three acceptance tests locally

--- public/index.php
+++ public/index.php
@@ -1,10 +1,35 @@
 <?php
 require '../vendor/autoload.php';
 
+R::setup(
+    'mysql:host=localhost;dbname=pong_matcher_slim_development',
+    'slimpong', 'slimpong'
+);
+
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name', function ($name) {
-        echo "Hello, $name";
+$app->delete('/all', function() {
+    R::wipe('matchrequest');
+});
+
+$app->put('/match_requests/:uuid', function($uuid) use($app) {
+    $matchRequest = R::dispense('matchrequest');
+    $attributes = json_decode($app->request->getBody());
+    $matchRequest->uuid = $uuid;
+    $matchRequest->player = $attributes->player;
+    R::store($matchRequest);
+    echo "{}";
+});
+
+$app->get('/match_requests/:uuid', function($uuid) {
+    $matchRequest = R::findOne('matchrequest', "uuid = ?", [ $uuid ]);
+    if ($matchRequest) {
+        echo json_encode([
+            'id' => $matchRequest->uuid,
+            'player' => $matchRequest->player,
+            'match_id' => $matchRequest->matchId
+        ]);
+    }
 });
 
 $app->run();
